3b0bf815984cd6d1829c133ec507b30bbba14c20
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-09-08 17:34:12 +0100

    large prompt improvement

--- syntax.py
+++ syntax.py
@@ -6,7 +6,7 @@ import multiprocessing
 
 # Globals
 llm = None
-TEST_CONTENT_LARGE = "Your name is Mr. Llama, your role is Chatbot to Human. You are at half-way up a mountain, where you and Human are present, and Human has just stated, Hello I though I'd go up the mountain for a while, fancy meeting you here, how are you doing today?, to you. Your task is, in one sentence, to respond to Humans's statement, in a method that are in context with, your emotional state being Indifferent, as well as the recent events are that, The conversation Started."
+TEST_CONTENT_LARGE = "Your name is Mr. Llama, your role is Chatbot to Human. In the location of half-way up a mountain, you and Human, are present. Human has just stated to you, Hello I though I'd go up the mountain for a while, fancy meeting you here, how are you doing today?. Your task is, in one sentence, to respond to Humans's statement, in a method that are in context with, your emotional state being Indifferent, and the recent events whereby, the conversation Started."
 TEST_CONTENT_DEFAULT_SMALL = "Hello Mr. Llama, nice to see you, care to shoot the breeze?!"
 TEMPERATURE_DEFAULT = 0.5
 CONTEXT_LENGTH = 4096
